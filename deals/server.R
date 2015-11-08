3137bcb56292bf103629d28fd4e4c9a27ea5f1ae
spa23 <stefan.avey@yale.edu>
2015-11-07 21:11:30 -0500

    gave up on 'from' to 'to' filtering

--- deals/server.R
+++ deals/server.R
@@ -4,8 +4,70 @@
 addTooltip(session, id="origin", title = "This is an input.",
                       placement = "left", trigger = "hover")
 
+
+## Determine the appropriate destination ("To") fields based on the origin ("From")
+observe({
+  ## If no origin is selected, show everything
+  if(is.null(input$origin)) {
+    choices <- sort(unique(c(as.character(dat$Fare$Destination),
+                             as.character(dat$MarketTable$GeographicRegionName),
+                             as.character(dat$MarketTable$MarketGroupName))
+                           ))
+    
+  } else {                              # some origin is selected
+    codes <- as.character(unique(unlist(
+      filter(dat$Fare, Origin %in% input$origin) %>% select(Destination))))
+    regions <- filter(dat$MarketTable, AirportCode %in% codes) %>%
+      select(GeographicRegionName, MarketGroupName)
+    regions <- as.character(unique(unlist(regions)))
+    choices <- sort(c(codes, regions))
+  }
+  output$dest <- renderUI({selectInput("dest", "To", multiple=TRUE, choices=choices)})
+})
+
+## Determine the appropriate origin ("From") fields based on the destination ("To")
+observe({
+  ## ## If no destination is selected, show everything
+  ## if(is.null(input$dest)) {
+    choices <- sort(unique(c(as.character(dat$Fare$Origin),
+                             as.character(dat$MarketTable$GeographicRegionName),
+                             as.character(dat$MarketTable$MarketGroupName))
+                           ))
+    
+  ## } else {                   # some destination is selected so do some filtering
+  ##   codes <- c()
+  ##   for (destination in input$dest) {
+  ##     ## Determine the type that is being considered
+  ##     id <- data.frame(t(apply(marketTable[, c("AirportCode", "GeographicRegionName",
+  ##                                              "MarketGroupName")], 2, function(x) {
+  ##                                                destination[i] %in% x
+  ##                                              })))
+
+  ##     ## Switch: AirportCode
+  ##     if (id["AirportCode"] == TRUE) {
+  ##       search <- destination
+  ##       ## Case: GeographicRegion
+  ##     } else if (id["GeographicRegionName"] == TRUE) {
+  ##       codes1 <- filter(dat$MarketTable, AirportCode %in% destination) %>%
+  ##         select(GeographicRegionName)
+  ##       search <- 
+  ##       ## Case: MarketGroup
+  ##     } else if (id["MarketGroupName"] == TRUE) {
+  ##       rankList[[i]] <- MarketScore(bound[i], type, flights, marketTable)
+  ##     }
+  ##     codes <- c(codes, as.character(unique(unlist(
+  ##       filter(dat$Fare, Destination %in% destination) %>% select(Origin)))))
+      
+  ##   }                                 # end for over length of input$dest
+  ##   choices <- sort(codes)
+  ## }
+  output$dest <- renderUI({selectInput("origin", "From", multiple=TRUE, choices=choices)})
+})
+
+
+
 ## Determine how budget should show dollars vs points
-observe( {
+observe({
   if(input$pointsFlag) {
     fares <<- filter(dat$Fares, FareType == "POINTS")
     ## Points should not include tax
